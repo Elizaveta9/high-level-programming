9c58a8af9207de9fd475be4bc58670f86813a843
Елизавета <katkova_lizochka@bk.ru>
2024-09-20 21:36:29 +0300

    Выполнено 3-ое задание

---
+++ week1/task3/Dodecahedron.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Dodecahedron {
+	public static void main(String[] args){
+		int a = getA();
+		printV(a);
+	}
+	// Получить сторону додекаэдра
+	public static int getA(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Введите сторону додекаэдра: ");
+		int a = sc.nextInt();
+		return a;
+	}
+	// Рассчиать объем додекаэдра
+	public static void printV(int a) {
+		double V = (15 + 7 * Math.sqrt(5) / 4 * Math.pow(a, 3));
+		System.out.println("Объем = " + V);
+	}
+}
