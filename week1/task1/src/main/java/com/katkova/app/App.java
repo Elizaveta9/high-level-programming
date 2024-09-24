11172781e5ff99d4ce157504f86875e53efeeb00
Елизавета <katkova_lizochka@bk.ru>
2024-09-24 23:52:32 +0300

    Задача 1 полностью переделана под maven

--- week1/task1/src/main/java/com/katkova/app/App.java
+++ week1/task1/src/main/java/com/katkova/app/App.java
@@ -1,10 +1,20 @@
 package com.katkova.app;
 
-/**
- * Hello world!
- */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+import java.util.Scanner;
+class App {
+	public static void main(String[] args){
+		String name = getUserName();
+		printHello(name);
+	}
+	// Запрашивает у пользователя имя
+	public static String getUserName(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Введите имя: ");
+		String name = sc.nextLine();
+		return name;
+	}
+	// Выводит приветствие
+	public static void printHello(String name){
+		System.out.printf("Привет, %s!\n", name); 
+	}
 }
